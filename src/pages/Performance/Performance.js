fa5950e9bd4911e9c01a415d17de8cbdeef3788b
joesong7 <72059440+joesong77@users.noreply.github.com>
2023-04-28 14:03:23 +0800

    20230427 修正

--- src/pages/Performance/Performance.js
+++ src/pages/Performance/Performance.js
@@ -11,37 +11,37 @@ function Proformance() {
         {
             id: 1,
             image: "https://i.imgur.com/rzHBV8V.jpg",
-            caseName: "九區堂營區",
+            caseName: "陸軍八軍團-九曲堂營區",
         },
         {
             id: 2,
             image: "https://i.imgur.com/W2SC2mj.jpg",
-            caseName: "大漢營區",
+            caseName: "陸軍司令部教準部-大漢營區",
         },
         {
             id: 3,
             image: "https://i.imgur.com/8COfysH.jpg",
-            caseName: "后里營區",
+            caseName: "軍備局-后里營區",
         },
         {
             id: 4,
             image: "https://i.imgur.com/Goq8A2L.jpg",
-            caseName: "育勤營區",
+            caseName: "軍備局-育勤二營區",
         },
         {
             id: 5,
             image: "https://i.imgur.com/dIbsGSJ.jpg",
-            caseName: "屏北暨太麻里",
+            caseName: "海軍-屏北暨太麻里營區",
         },
         {
             id: 6,
             image: "https://i.imgur.com/iR433T1.jpg",
-            caseName: "凌雲崗營區",
+            caseName: "陸軍六軍團-凌雲崗營區",
         },
         {
             id: 7,
             image: "https://i.imgur.com/2URxvPc.jpg",
-            caseName: "淡水營區",
+            caseName: "陸軍六軍團-淡水營區",
 
         },
 
@@ -70,7 +70,8 @@ function Proformance() {
                                 srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
                                 alt={item.caseName}
                                 loading="lazy"
-                         
+                                width={400}
+                                height={150}
 
                             />
                             <ImageListItemBar
