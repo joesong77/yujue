3fae799388896562445a99ed3e64cd70cf276897
joesong7 <72059440+joesong77@users.noreply.github.com>
2025-05-12 19:36:28 +0800

    刪除實績的router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AppBar from "./components/AppBar/AppBar"
 import BottomNavigation from "./components/BottomNavigation/BottomNavigation";
 import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
-import Proformance from "./pages/Performance/Performance";
+
 import SocialFeedback from "./pages/SocialFeedback/SocialFeedback";
 import ServiceItem from "./pages/ServiceItem/ServiceItem";
 
@@ -29,9 +29,7 @@ function App() {
           <Routes>
             <Route path="/about" element={<About />} />
           </Routes>
-          <Routes>
-            <Route path="/proformance" element={<Proformance />} />
-          </Routes>
+         
           <Routes>
             <Route path="/socialFeedback" element={<SocialFeedback />} />
           </Routes>
